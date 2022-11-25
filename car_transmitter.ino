631dfc16fca1d66f79232f809ee3f424f38f3488
Chrisjubin <117363772+Chrisjubin@users.noreply.github.com>
2022-11-26 00:17:43 +0530

    Change to Bluetooth (2nd last commit)
    rf has been changed to Bluetooth because of range problems.
    changed a few bytes for the my serial connection.
    usltrig and uslecho changed to 13 and 12 from 12 and 10.
    removed the library for rf modules
    
    The transmitter code has been changed to Bluetooth code.
    IN pins are also analog now.

--- car_transmitter.ino
+++ car_transmitter.ino
@@ -1,65 +1,60 @@
-// Include RadioHead Amplitude Shift Keying Library
-#include <RH_ASK.h>
-// Include dependant SPI Library
-#include <SPI.h>
+/* How to configure and pair two HC-05 Bluetooth Modules 
+*               == MASTER CODE ==                
+*/
 
-const byte front = 6;
-const byte back = 7;
-const byte right = 8;
-const byte left = 9;
-
-byte motonum = 0;
-
-// Create Amplitude Shift Keying Object
-RH_ASK rf_driver;
-
-void setup() {
-  pinMode(front, INPUT_PULLUP);
-  pinMode(back, INPUT_PULLUP);
-  pinMode(right, INPUT_PULLUP);
-  pinMode(left, INPUT_PULLUP);
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(8, 9); // RX, TX
+ 
 
 
-  // Initialize ASK Object
-  rf_driver.init();
+#define button A0
+#define button2 A1
+#define button3 A2
+#define button4 A3
+
+
+int buttonstate;
+int buttonstate2;
+int buttonstate3;
+int buttonstate4;
+
+void setup() { // put your setup code here, to run once
+ pinMode(button, INPUT_PULLUP);
+ pinMode(button2, INPUT_PULLUP);
+pinMode(button3, INPUT_PULLUP);
+pinMode(button4, INPUT_PULLUP);
+
+ Serial.begin(9600); // Begin serial communication with Arduino and Arduino IDE (Serial Monitor) 
+ mySerial.begin(9600); // Default communication rate of the Bluetooth module
+
 }
 
-void loop() {
+void loop() { 
+ // Reading the button
+ buttonstate = digitalRead(button);
+ buttonstate2 = digitalRead(button2);
+ buttonstate3 = digitalRead(button3);
+ buttonstate4 = digitalRead(button4);
 
-  if (digitalRead(front) == LOW) {
-    motonum = 1;
-  }
+ if (digitalRead(button) == LOW) { //send  data to slave
+  mySerial.write('1'); 
+ }
+ else if (digitalRead(button2) == LOW)
+ {
+   mySerial.write('2');
+ }  
+ else if (digitalRead(button3) == LOW)
+ {
+   mySerial.write('3');
+ } 
+ else if (digitalRead(button4) == LOW)
+ {
+   mySerial.write('4');
+ }
+ else
+ {
+   mySerial.write('0');
+ }  
 
-
-
-
-  if (digitalRead(back) == LOW) {
-    motonum = 2;
-  }
-
-
-
-
-  if (digitalRead(right) == LOW) {
-    motonum = 3;
-  }
-
-
-
-  if (digitalRead(left) == LOW) {
-    motonum = 4;
-  }
-
-  if (digitalRead(left) == HIGH && digitalRead(right) == HIGH && digitalRead(back) == HIGH && digitalRead(front) == HIGH) {
-    motonum = 0;
-  }
-
-
-
-
-
-  char *msg = motonum;
-  rf_driver.send((uint8_t *)&motonum, sizeof motonum);
-  rf_driver.waitPacketSent();
-  delay(50);
 }
+
